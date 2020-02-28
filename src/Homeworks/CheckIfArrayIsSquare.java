ef5281eab8ac427f2467dc077fe30db6381d8fd6
ElenaIvanov <ivanovelenaiulia@yahoo.com>
2020-02-28 19:38:39 +0200

    Week 2 homework

---
+++ src/Homeworks/CheckIfArrayIsSquare.java
@@ -0,0 +1,20 @@
+package com.elenaivanov.repositoryforhomework;
+
+public class CheckIfArrayIsSquare {
+
+    public static void main(String[] args) {
+
+        int[][] matrix1 = {
+                {4, 4, 3},
+                {5, 7, 4},
+                {7, 8, 9}
+        };
+
+        System.out.println();
+    }
+
+
+    public static boolean isSquare(int[][] array) {
+        for(int i = 0; )
+    }
+}
