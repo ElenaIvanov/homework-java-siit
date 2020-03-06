f133141ddf914655b3ece0d4f6cccced8b402f24
ElenaIvanov <ivanovelenaiulia@yahoo.com>
2020-03-06 23:27:29 +0200

    Week 3 homework

---
+++ src/main/java/com/siit/homeworks/week03/MethodFirst100PrimeNo.java
@@ -0,0 +1,30 @@
+package com.siit.homeworks.week03;
+
+public class MethodFirst100PrimeNo {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int count = 0;
+        int number = 0;
+
+        while (number <= 100) {
+            if (isPrime(number)) {
+                sum += number;
+                count++;
+            }
+            number++;
+        }
+
+        System.out.println("The sum of the prime numbers until 100 is: " + sum);
+    }
+
+    public static boolean isPrime(int number) {
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
