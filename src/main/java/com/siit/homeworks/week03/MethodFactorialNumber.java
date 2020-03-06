b1a434f2e7521a63ab898747ffe8d8334bce5a00
ElenaIvanov <ivanovelenaiulia@yahoo.com>
2020-03-06 22:32:19 +0200

    Week 3 homework

---
+++ src/main/java/com/siit/homeworks/week03/MethodFactorialNumber.java
@@ -0,0 +1,29 @@
+package com.siit.homeworks.week03;
+
+import java.util.Scanner;
+
+public class MethodFactorialNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input the number you want to calculate the factorial!:");
+
+        int factorial = scanner.nextInt();
+
+        int sumFac = sumFactorial(factorial);
+        System.out.println("The factorial of the given number is " + sumFac);
+
+
+    }
+
+    public static int sumFactorial(int factorial) {
+        int result = 1;
+
+        for (int factor = 2; factor <= factorial; factor++) {
+            result *= factor;
+        }
+
+        return result;
+    }
+}
