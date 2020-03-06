875d85a9f7dddd95094f5fb17525fb6a92f6825b
ElenaIvanov <ivanovelenaiulia@yahoo.com>
2020-03-06 23:05:19 +0200

    Week 3 homework

---
+++ src/main/java/com/siit/homeworks/week03/MethodMaxDigitOfNumber.java
@@ -0,0 +1,29 @@
+package com.siit.homeworks.week03;
+
+import java.util.Scanner;
+
+public class MethodMaxDigitOfNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the number: ");
+
+        int number = scanner.nextInt();
+
+        System.out.println("The maximum digit in the number is " + maximum(number));
+    }
+
+    private static int maximum(int number) {
+        int output=0;
+        if (number > 0) {
+            int max = 0;
+
+            while ((number > 0)) {
+                int digit = number % 10;
+                max = Math.max(max, digit);
+                number /= 10;
+            }
+            output = max;
+        }
+        return output;
+    }
+}
