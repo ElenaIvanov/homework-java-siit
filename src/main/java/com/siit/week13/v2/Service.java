fd9f7316875cd2576d090be20fa4d5c31780bc21
ElenaIvanov <ivanovelenaiulia@yahoo.com>
2020-06-05 22:20:54 +0300

    Week 13 homework

--- src/main/java/com/siit/week13/v2/Service.java
+++ src/main/java/com/siit/week13/v2/Service.java
@@ -2,15 +2,14 @@ package com.siit.week13.v2;
 
 import java.util.Random;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Service {
     private static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
         FestivalGate gate = new FestivalGate();
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors
-                .newFixedThreadPool(5);
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(6);
         {
             for (int i = 0; i < 1000; i++) {
                 TicketType ticketType = Service.randomTicket();
